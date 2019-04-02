fdf2d0899cca8b176d97856d39d7a47df52a7202
Martin Gencur <mgencur@redhat.com>
2019-04-02 10:38:13 +0200

    Specify namespace for oc exec

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -335,7 +335,7 @@ function run_origin_e2e() {
   
   timeout 60 "oc get pods -n knative-eventing | grep e2e-origin-testsuite | grep -E 'Running'"
   e2e_origin_pod=$(oc get pods -n knative-eventing | grep e2e-origin-testsuite | grep -E 'Running' | awk '{print $1}')
-  timeout 3600 "oc exec $e2e_origin_pod -c test ls /tmp/artifacts/e2e-origin/test_logs.tar"
+  timeout 3600 "oc -n knative-eventing exec $e2e_origin_pod -c test ls /tmp/artifacts/e2e-origin/test_logs.tar"
   oc cp knative-eventing/${e2e_origin_pod}:/tmp/artifacts/e2e-origin/test_logs.tar .
   tar xvf test_logs.tar
 }
