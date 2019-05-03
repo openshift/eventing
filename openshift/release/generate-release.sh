2bc28559ffcf7a4070281eca009754701e0e59a7
Alan Field <afield@redhat.com>
2019-05-03 11:05:38 -0400

    Update openshift specific files.

---
+++ openshift/release/generate-release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source $(dirname $0)/resolve.sh
+
+release=$1
+
+quay_image_prefix="quay.io/openshift-knative/knative-eventing-"
+output_file="openshift/release/knative-eventing-${release}.yaml"
+
+resolve_resources config/ $output_file $quay_image_prefix $release
+
+# in-memory-channel
+resolve_resources config/provisioners/in-memory-channel/ channel-resolved.yaml $quay_image_prefix $release
+cat channel-resolved.yaml >> $output_file
+rm channel-resolved.yaml
+
+# Apache Kafka channel
+# resolve_resources contrib/kafka/config/ kafka-resolved.yaml $quay_image_prefix $release
+# cat kafka-resolved.yaml >> $output_file
+# rm kafka-resolved.yaml
