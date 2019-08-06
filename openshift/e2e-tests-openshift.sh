d4163267f3e9e48cf7126038250d0662eb7b9ec0
Matthias Wessendorf <mwessend@redhat.com>
2019-08-06 16:53:20 +0200

    timeouts...

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -227,7 +227,7 @@ function run_e2e_tests(){
   options=""
   (( EMIT_METRICS )) && options="-emitmetrics"
   report_go_test \
-    -v -tags=e2e -count=1 -timeout=20m -short -parallel=1 \
+    -v -tags=e2e -count=1 -timeout=35m -short -parallel=1 \
     ./test/e2e \
     --kubeconfig $KUBECONFIG \
     --dockerrepo ${INTERNAL_REGISTRY}/${EVENTING_NAMESPACE} \
