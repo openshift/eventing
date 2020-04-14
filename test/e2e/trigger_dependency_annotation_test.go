903cf5ca32a67512302d0c8750572f7acc9180b3
Matthias Wessendorf <mwessend@redhat.com>
2020-04-14 14:13:17 +0200

    Now, we can run the test...
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/e2e/trigger_dependency_annotation_test.go
+++ test/e2e/trigger_dependency_annotation_test.go
@@ -38,7 +38,6 @@ import (
 // Broker controller should make trigger become ready after pingSource is ready.
 // This trigger dependency annotation is related on issue #1734.
 func TestTriggerDependencyAnnotation(t *testing.T) {
-	t.Skip("skipping")
 	const (
 		defaultBrokerName    = pkgResources.DefaultBrokerName
 		triggerName          = "trigger-annotation"
