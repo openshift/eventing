f7ae596db1875b65ecdd48e25f1ae3b6064c0dad
Marek Schmidt <maschmid@redhat.com>
2021-05-03 16:52:40 +0200

    create instead of apply in strimzi

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -56,10 +56,10 @@ function install_strimzi(){
   kubectl create namespace kafka
   curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
   | sed 's/namespace: .*/namespace: kafka/' \
-  | kubectl -n kafka apply -f -
+  | kubectl -n kafka create -f -
 
   header_text "Applying Strimzi Cluster file"
-  kubectl -n kafka apply -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
+  kubectl -n kafka create -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
 
   header_text "Waiting for Strimzi to become ready"
   sleep 5; while echo && kubectl get pods -n kafka | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
