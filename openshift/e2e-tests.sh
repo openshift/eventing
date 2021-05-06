275c11608c6162b8394af053ffa907217e596c2f
Matthias Wessendorf <mwessend@redhat.com>
2021-05-06 12:35:50 +0200

    Let's just run the REKT TESTS
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -20,9 +20,9 @@ failed=0
 
 (( !failed )) && install_tracing || failed=1
 
-(( !failed )) && run_e2e_tests || failed=1
+# (( !failed )) && run_e2e_tests || failed=1
 
-(( !failed )) && run_conformance_tests || failed=1
+# (( !failed )) && run_conformance_tests || failed=1
 
 (( !failed )) && run_e2e_rekt_tests || failed=1
 
